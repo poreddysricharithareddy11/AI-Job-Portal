5c43289e81dfb87820ec613d3ce1a34f6da494fc
poreddysricharithareddy11 <157510068+poreddysricharithareddy11@users.noreply.github.com>
2026-04-20 12:45:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+// Fallback URL (safety for production)
+const BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://ai-job-portal-backend-0acj.onrender.com";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL: BASE_URL,
 });
 
+// Attach token automatically
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
